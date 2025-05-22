984d5da54f97ea8cdcc5d341f30e07c4f5276499
Fernando Rocha <A00836634@tec.mx>
2025-05-21 20:20:50 -0600

    Actualizacion 1.9

--- test/test/test_palindromo.cpp
+++ test/test/test_palindromo.cpp
@@ -10,3 +10,28 @@ TEST(PalindromoTest, DetectaCorrectamente)
     auto [inicio, fin, pal] = encontrarPalindromoMasLargo(entrada);
     EXPECT_EQ(pal, "abcddcba");
 }
+
+TEST(PalindromoTest, DetectaPalindromoCentral)
+{
+    string texto = "abcxyzyxdef";
+    auto [start, end, sub] = encontrarPalindromoMasLargo(texto);
+    EXPECT_EQ(sub, "xyzyx");
+    EXPECT_EQ(start, 4);
+    EXPECT_EQ(end, 9);
+}
+
+TEST(PalindromoTest, DetectaPalindromoCorto)
+{
+    string texto = "abac";
+    auto [start, end, sub] = encontrarPalindromoMasLargo(texto);
+    EXPECT_EQ(sub, "aba");
+    EXPECT_EQ(start, 1);
+    EXPECT_EQ(end, 3);
+}
+
+TEST(PalindromoTest, PalindromoUnCaracter)
+{
+    string texto = "xyz";
+    auto [start, end, sub] = encontrarPalindromoMasLargo(texto);
+    EXPECT_EQ(sub.length(), 1);
+}
